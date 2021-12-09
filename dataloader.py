089ecbbe613a616b04441f0d3b267438ac1baf31
Simon Hua <455215897@qq.com>
2021-12-08 23:45:10 -0500

    yuhua[update]

--- dataloader.py
+++ dataloader.py
@@ -148,9 +148,11 @@ train_transform = {
                 transforms.ToTensor()                                       
         ]),
         'stl10': transforms.Compose([
-                transforms.RandomCrop(32),
-                transforms.RandomHorizontalFlip(),
-                transforms.ToTensor()                                       
+              transforms.RandomResizedCrop(256),
+              transforms.RandomHorizontalFlip(),
+              transforms.CenterCrop(224),
+              transforms.ToTensor(),
+            #   transforms.Normalize([0.485, 0.456, 0.406],[0.229, 0.224, 0.225])
         ]),
         }
 
