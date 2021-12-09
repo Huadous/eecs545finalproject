248e66a0710bcf2fe88a7d0a005f159251d7d386
Simon Hua <455215897@qq.com>
2021-12-09 13:47:16 -0500

    yuhua[update]

--- dataloader.py
+++ dataloader.py
@@ -334,7 +334,7 @@ train_transform = {
                 # transforms.Normalize(*meanstd['cifar10'])
         ]),
         'fmnist': transforms.Compose([
-                # transforms.RandomCrop(32, padding=4),
+                transforms.RandomCrop(32, padding=4),
                 transforms.RandomHorizontalFlip(),
                 transforms.ToTensor()
                 # transforms.Normalize(*meanstd['cifar10'])
