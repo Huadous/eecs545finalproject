f3b1fc3137869ed29732c5e9e1f001665b5df36b
Simon Hua <455215897@qq.com>
2021-12-08 22:50:28 -0500

    yuhua[update]

---
+++ dataloader.py
@@ -0,0 +1,221 @@
+import math
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+from torch.optim import SGD
+from torch.utils.data import Dataset, DataLoader
+import torchvision.datasets as dsets
+import torchvision.transforms as transforms
+from PIL import Image
+import os, random, pickle
+from os.path import join, isfile
+from tqdm import tqdm
+import numpy as np
+
+def get_class_balanced_labels(targets, labels_per_class, save_path=None):
+    num_classes = max(targets) + 1
+
+    indices = list(range(len(targets)))
+    random.shuffle(indices)
+
+    label_count = {i: 0 for i in range(num_classes)}
+    label_indices, unlabel_indices = [], []
+    for idx in indices:
+        if label_count[targets[idx].item()] < labels_per_class:
+            label_indices.append(idx)
+            label_count[targets[idx].item()] += 1
+        else:
+            unlabel_indices.append(idx)
+
+    if save_path is not None:
+        with open(join(save_path, 'label_indices.txt'), 'w+') as f:
+            for idx in label_indices:
+                f.write(str(idx) + '\n')
+
+    return label_indices, unlabel_indices
+
+
+def get_repeated_indices(indices, num_iters, batch_size):
+    length = num_iters * batch_size
+    num_epochs = length // len(indices) + 1
+    repeated_indices = []
+
+    for epoch in tqdm(range(num_epochs), desc='Pre-allocating indices'):
+        random.shuffle(indices)
+        repeated_indices += indices
+
+    return repeated_indices[:length]
+
+class MNIST(dsets.MNIST):
+    num_classes = 10
+    def __init__(self, num_labels, num_iters, batch_size, return_unlabel=True, save_path=None, **kwargs):
+        super(MNIST, self).__init__(**kwargs)
+        labels_per_class = num_labels // self.num_classes
+        self.return_unlabel = return_unlabel
+
+        self.label_indices, self.unlabel_indices = get_class_balanced_labels(self.targets, labels_per_class, save_path)
+        self.repeated_label_indices = get_repeated_indices(self.label_indices, num_iters, batch_size)
+        if self.return_unlabel:
+            self.repeated_unlabel_indices = get_repeated_indices(self.unlabel_indices, num_iters, batch_size)
+
+    def __len__(self):
+        return len(self.repeated_label_indices)
+
+    def __getitem__(self, idx):
+        label_idx = self.repeated_label_indices[idx]
+        label_img, label_target = self.data[label_idx], self.targets[label_idx]
+        label_img = np.asarray(label_img)
+        print(type(label_img))
+        label_img = Image.fromarray(label_img)
+        
+
+        if self.transform is not None:
+            label_img = self.transform(label_img)
+        if self.target_transform is not None:
+            label_target = self.target_transform(label_target)
+
+        if self.return_unlabel:
+            unlabel_idx = self.repeated_unlabel_indices[idx]
+            unlabel_img, unlabel_target = self.data[unlabel_idx], self.targets[unlabel_idx]
+            unlabel_img = np.asarray(unlabel_img)
+            unlabel_img = Image.fromarray(unlabel_img)
+
+            if self.transform is not None:
+                unlabel_img = self.transform(unlabel_img)
+            if self.target_transform is not None:
+                unlabel_target = self.target_transform(unlabel_target)
+            return label_img, label_target, unlabel_img, unlabel_target
+        else:
+            return label_img, label_target
+
+
+class iNaturalist(dsets.inaturalist.INaturalist):
+    def __init__(self, num_labels, num_iters, batch_size, return_unlabel=True, save_path=None, **kwargs):
+        super(iNaturalist, self).__init__(**kwargs)
+        # labels_per_class = num_labels // self.num_classes
+        # self.return_unlabel = return_unlabel
+
+        # self.label_indices, self.unlabel_indices = get_class_balanced_labels(self.targets, labels_per_class, save_path)
+        # self.repeated_label_indices = get_repeated_indices(self.label_indices, num_iters, batch_size)
+        # if self.return_unlabel:
+        #     self.repeated_unlabel_indices = get_repeated_indices(self.unlabel_indices, num_iters, batch_size)
+
+class STL10(dsets.STL10):
+    num_classes = 10
+    def __init__(self, num_labels, num_iters, batch_size, return_unlabel=True, save_path=None, **kwargs):
+        super(STL10, self).__init__(**kwargs)
+        labels_per_class = num_labels // self.num_classes
+        self.return_unlabel = return_unlabel
+
+        self.label_indices, self.unlabel_indices = get_class_balanced_labels(self.labels, labels_per_class, save_path)
+        self.repeated_label_indices = get_repeated_indices(self.label_indices, num_iters, batch_size)
+        if self.return_unlabel:
+            self.repeated_unlabel_indices = get_repeated_indices(self.unlabel_indices, num_iters, batch_size)
+
+    def __getitem__(self, idx):
+        label_idx = self.repeated_label_indices[idx]
+        label_img, label_target = self.data[label_idx], int(self.labels[label_idx])
+        label_img = Image.fromarray(np.transpose(label_img, (1, 2, 0)))
+
+        if self.transform is not None:
+            label_img = self.transform(label_img)
+        if self.target_transform is not None:
+            label_target = self.target_transform(label_target)
+
+        if self.return_unlabel:
+            unlabel_idx = self.repeated_unlabel_indices[idx]
+            unlabel_img, unlabel_target = self.data[unlabel_idx], int(self.labels[unlabel_idx])
+            unlabel_img = Image.fromarray(np.transpose(unlabel_img, (1, 2, 0)))
+
+            if self.transform is not None:
+                unlabel_img = self.transform(unlabel_img)
+            if self.target_transform is not None:
+                unlabel_target = self.target_transform(unlabel_target)
+            return label_img, label_target, unlabel_img, unlabel_target
+        else:
+            return label_img, label_target
+
+train_transform = {
+        'mnist': transforms.Compose([
+                transforms.RandomCrop(32, padding=4),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor()
+                # transforms.Normalize(*meanstd['cifar10'])
+        ]),
+        'inaturalist': transforms.Compose([
+                transforms.RandomCrop(32),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor()                                       
+        ]),
+        'stl10': transforms.Compose([
+                transforms.RandomCrop(32),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor()                                       
+        ]),
+        }
+
+
+
+train_dset = {
+        'mnist': MNIST,
+        'inaturalist': iNaturalist,
+        'stl10': STL10,
+        }
+
+test_dset = {
+        'mnist': dsets.MNIST,
+        'inaturalist': dsets.inaturalist.INaturalist,
+        'stl10': dsets.STL10,
+        }
+
+train_kwargs = {
+        'mnist': {'train': True, 'download': True},
+        'inaturalist': {'version': '2021_train_mini', 'target_type': 'class', 'train': True, 'download': True},
+        'stl10': {'split': 'train', 'download': True},
+        }
+
+test_kwargs = {
+        'mnist': {'train': False, 'download': True},
+        'inaturalist': {'version': '2021_train_mini', 'target_type': 'class', 'train': True, 'download': True},
+        'stl10': {'split': 'test', 'download': True},
+        }
+
+def dataloader1(dset, path, bs, num_workers, num_labels, num_iters, return_unlabel=True, save_path=None):
+    assert dset in ["mnist", "inaturalist", "stl10"]
+
+    train_dataset = train_dset[dset](
+            root = path,
+            num_labels = num_labels,
+            num_iters = num_iters,
+            batch_size = bs,
+            return_unlabel = return_unlabel,
+            transform = train_transform[dset],
+            save_path = save_path,
+            **train_kwargs[dset]
+    )
+    train_loader = DataLoader(train_dataset, batch_size=bs, num_workers=num_workers, shuffle=False)
+
+    test_transform = transforms.Compose([
+            transforms.ToTensor()
+            # transforms.Normalize(*meanstd[dset])
+    ])
+    test_dataset = test_dset[dset](root=path, transform=test_transform, **test_kwargs[dset])
+    test_loader = DataLoader(test_dataset, batch_size=100, num_workers=num_workers, shuffle=False)
+
+    return iter(train_loader), test_loader
+
+train_loader, test_loader = dataloader1(
+        dset = 'stl10',
+        path = '/content/drive/MyDrive/EECS545FINAL',
+        bs = 1024,
+        num_workers = 8,
+        num_labels = 2000,
+        num_iters = 40000,
+        return_unlabel = True,
+        save_path = '/content/drive/MyDrive/EECS545FINAL'
+)
+
+a,b,c,d = next(train_loader)
+a
+
+next(train_loader)
---
+++ train_meta.py
@@ -0,0 +1,293 @@
+import os, argparse, torch, math, time, random
+from os.path import join, isfile
+import torch.nn.functional as F
+from torch.optim import SGD
+from torch.distributions import Beta
+from tensorboardX import SummaryWriter
+
+from dataloader import dataloader1
+from utils import make_folder, AverageMeter, Logger, accuracy, save_checkpoint, compute_weight
+from model import ConvLarge
+
+parser = argparse.ArgumentParser()
+# Basic configuration
+parser.add_argument('--dataset', type=str, default='mnist', choices=['mnist', 'caltech256', 'svhn'])
+parser.add_argument('--data-path', type=str, default='./data', help='Data path')
+parser.add_argument('--num-label', type=int, default=4000, help='Number of labeled data')
+parser.add_argument('-a', '--architecture', type=str, default='convlarge', choices=['convlarge', 'vgg16'], help='Network architecture')
+parser.add_argument('--mix-up', action='store_true', help='Use mix-up augmentation')
+parser.add_argument('--alpha', type=float, default=1., help='Concentration parameter of Beta distribution')
+parser.add_argument('--weight', type=float, default=1., help='re-weighting scalar for the additional loss')
+# Training setting
+parser.add_argument('--total-steps', type=int, default=50000, help='Start step (for resume)')
+parser.add_argument('--start-step', type=int, default=0, help='Start step (for resume)')
+parser.add_argument('--batch-size', type=int, default=128, help='Batch size')
+parser.add_argument('--epsilon', type=float, default=1e-2, help='epsilon for gradient estimation')
+parser.add_argument('--lr', type=float, default=0.1, help='Maximum learning rate')
+parser.add_argument('--warmup', type=int, default=4000, help='Warmup iterations')
+parser.add_argument('--gamma', type=float, default=0.1, help='Learning rate annealing multiplier')
+parser.add_argument('--milestones', type=eval, default=[300000, 350000], help='Learning rate annealing steps')
+parser.add_argument('--weight-decay', type=float, default=1e-4, help='Weight decay')
+parser.add_argument('--momentum', type=float, default=0.9, help='Momentum for SGD optimizer')
+parser.add_argument('--num-workers', type=int, default=4, help='Number of workers')
+parser.add_argument('--resume', type=str, default=None, help='Resume model from a checkpoint')
+parser.add_argument('--seed', type=int, default=1234, help='Random seed for reproducibility')
+parser.add_argument('--print-freq', type=int, default=100, help='Print and log frequency')
+parser.add_argument('--test-freq', type=int, default=400, help='Test frequency')
+parser.add_argument('--save-path', type=str, default='./results/tmp', help='Save path')
+parser.add_argument('--gpu', action='store_true', help='Use GPU')
+args = parser.parse_args()
+args.num_classes = 100 if args.dataset == 'cifar100' else 10
+
+# Set random seed
+random.seed(args.seed)
+torch.manual_seed(args.seed)
+if args.gpu:
+    torch.cuda.manual_seed_all(args.seed)
+os.environ['PYTHONHASHSEED'] = str(args.seed)
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = True
+
+# Create directories if not exist
+make_folder(args.save_path)
+logger = Logger(join(args.save_path, 'log.txt'))
+writer = SummaryWriter(log_dir=args.save_path)
+logger.info('Called with args:')
+logger.info(args)
+
+# Define dataloader
+logger.info("Loading data...")
+train_loader, test_loader = dataloader1(
+        dset = args.dataset,
+        path = args.data_path,
+        bs = args.batch_size,
+        num_workers = args.num_workers,
+        num_labels = args.num_label,
+        num_iters = args.total_steps,
+        return_unlabel = True,
+        save_path = args.save_path
+)
+
+# Build model and optimizer
+logger.info("Building model and optimizer...")
+if args.architecture == "convlarge":
+    model = ConvLarge(num_classes=args.num_classes)
+elif args.architecture == "vgg16":
+    model = torch.hub.load('pytorch/vision:v0.10.0', 'vgg16', pretrained = False)
+if args.gpu:
+    model.cuda()
+optimizer = SGD(model.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
+logger.info("Model:\n%s\nOptimizer:\n%s" % (str(model), str(optimizer)))
+
+# Optionally build beta distribution
+if args.mix_up:
+    beta_distribution = Beta(torch.tensor([args.alpha]), torch.tensor([args.alpha]))
+
+# Optionally resume from a checkpoint
+if args.resume is not None:
+    if isfile(args.resume):
+        logger.info("=> loading checkpoint '{}'".format(args.resume))
+        checkpoint = torch.load(args.resume)
+        args.start_step = checkpoint['step']
+        best_acc = checkpoint['best_acc']
+        model.load_state_dict(checkpoint['model'])
+        optimizer.load_state_dict(checkpoint['optimizer'])
+        logger.info("=> loaded checkpoint '{}' (step {})".format(args.resume, checkpoint['step']))
+    else:
+        logger.info("=> no checkpoint found at '{}'".format(args.resume))
+
+def compute_lr(step):
+    if step < args.warmup:
+        lr = args.lr * step / args.warmup
+    else:
+        lr = args.lr
+        for milestone in args.milestones:
+            if step > milestone:
+                lr *= args.gamma
+    return lr
+
+def main():
+    data_times, batch_times, label_losses, unlabel_losses, label_acc, unlabel_acc = [AverageMeter() for _ in range(6)]
+    if args.mix_up: interp_losses = AverageMeter()
+    best_acc = 0.
+    logger.info("Start training...")
+    for step in range(args.start_step, args.total_steps):
+        # Load data and distribute to devices
+        data_start = time.time()
+        label_img, label_gt, unlabel_img, unlabel_gt = next(train_loader)
+
+        if args.gpu:
+            label_img = label_img.cuda()
+            label_gt = label_gt.cuda()
+            unlabel_img = unlabel_img.cuda()
+            unlabel_gt = unlabel_gt.cuda()
+        _label_gt = F.one_hot(label_gt, num_classes=args.num_classes).float()
+        data_end = time.time()
+
+        # Compute learning rate and meta learning rate
+        lr = compute_lr(step)
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = lr
+        weight = compute_weight(args.weight, step, args.warmup)
+
+        ### First-order Approximation ###
+        _concat = lambda xs: torch.cat([x.view(-1) for x in xs])
+        # Evaluation mode
+        model.eval()
+        # Forward label data and perform backward pass
+        label_pred = model(label_img)
+        label_loss = F.cross_entropy(label_pred, label_gt, reduction='mean')
+        dtheta = torch.autograd.grad(label_loss, model.parameters(), only_inputs=True)
+
+        with torch.no_grad():
+            # Compute the unlabel pseudo-gt
+            unlabel_pred = model(unlabel_img)
+            unlabel_pseudo_gt = F.softmax(unlabel_pred, dim=1)
+            # Compute step size for first-order approximation
+            epsilon = args.epsilon / torch.norm(_concat(dtheta))
+
+            # Forward finite difference
+            for p, g in zip(model.parameters(), dtheta):
+                p.data.add_(g, alpha=epsilon)
+            unlabel_pred_pos = model(unlabel_img)
+            # Backward finite difference
+            for p, g in zip(model.parameters(), dtheta):
+                p.data.sub_(g, alpha=2.*epsilon)
+            unlabel_pred_neg = model(unlabel_img)
+            # Resume original params
+            for p, g in zip(model.parameters(), dtheta):
+                p.data.add_(g, alpha=epsilon)
+
+            # Compute (approximated) gradients w.r.t pseudo-gt of unlabel data
+            unlabel_grad = F.softmax(unlabel_pred_pos, dim=1) - F.softmax(unlabel_pred_neg, dim=1)
+            unlabel_grad.div_(epsilon)
+
+            # Update and normalize pseudo-labels
+            unlabel_pseudo_gt.sub_(unlabel_grad, alpha=lr)
+            torch.relu_(unlabel_pseudo_gt)
+            sums = torch.sum(unlabel_pseudo_gt, dim=1, keepdim=True)
+            unlabel_pseudo_gt /= torch.where(sums == 0., torch.ones_like(sums), sums)
+
+        # Training mode
+        model.train()
+        # First compute label loss
+        if args.mix_up:
+            # Adopt mix-up augmentation
+            with torch.no_grad():
+                alpha = beta_distribution.sample((args.batch_size,))
+                if args.gpu:
+                    alpha = alpha.cuda()
+                _alpha = alpha.view(-1, 1, 1, 1)
+                interp_img = (label_img * _alpha + unlabel_img * (1. - _alpha)).detach()
+                interp_pseudo_gt = (_label_gt * alpha + unlabel_pseudo_gt * (1. - alpha)).detach()
+            interp_pred = model(interp_img)
+            interp_loss = F.kl_div(F.log_softmax(interp_pred, dim=1), interp_pseudo_gt, reduction='batchmean')
+        else:
+            # Regular label loss
+            label_pred = model(label_img)
+            label_loss = F.cross_entropy(label_pred, label_gt, reduction='mean')
+
+        # Then compute unlabel loss with `unlabel_pseudo_gt`
+        unlabel_pred = model(unlabel_img)
+        unlabel_loss = torch.norm(F.softmax(unlabel_pred, dim=1)-unlabel_pseudo_gt, p=2, dim=1).pow(2).mean()
+        loss = interp_loss + weight * unlabel_loss if args.mix_up else label_loss + weight * unlabel_loss
+
+        # One SGD step
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # Compute accuracy
+        label_top1, = accuracy(label_pred, label_gt, topk=(1,))
+        unlabel_top1, = accuracy(unlabel_pred, unlabel_gt, topk=(1,))
+        # Update AverageMeter stats
+        data_times.update(data_end - data_start)
+        batch_times.update(time.time() - data_end)
+        label_losses.update(label_loss.item(), label_img.size(0))
+        unlabel_losses.update(unlabel_loss.item(), unlabel_img.size(0))
+        label_acc.update(label_top1.item(), label_img.size(0))
+        unlabel_acc.update(unlabel_top1.item(), unlabel_img.size(0))
+        if args.mix_up:
+            interp_losses.update(interp_loss.item(), label_img.size(0))
+
+        # Print and log
+        if step % args.print_freq == 0:
+            logger.info("Step {0:05d} Dtime: {dtimes.avg:.3f} Btime: {btimes.avg:.3f} "
+                        "Lloss: {llosses.val:.3f} (avg {llosses.avg:.3f}) Uloss: {ulosses.val:.3f} (avg {ulosses.avg:.3f}) "
+                        "Lacc: {label.val:.3f} (avg {label.avg:.3f}) Uacc: {unlabel.val:.3f} (avg {unlabel.avg:.3f}) "
+                        "LR: {1:.4f}".format(step, lr, dtimes=data_times, btimes=batch_times, llosses=label_losses,
+                                             ulosses=unlabel_losses, label=label_acc, unlabel=unlabel_acc))
+
+        # Test and save model
+        if (step + 1) % args.test_freq == 0 or step == args.total_steps - 1:
+            acc = evaluate(test_loader, model)
+
+            # Remember best accuracy and save checkpoint
+            is_best = acc > best_acc
+            if is_best:
+                best_acc = acc
+            logger.info("Best Accuracy: %.5f" % best_acc)
+            save_checkpoint({
+                    'step': step + 1,
+                    'model': model.state_dict(),
+                    'best_acc': best_acc,
+                    'optimizer': optimizer.state_dict()
+            }, is_best, path=args.save_path, filename="checkpoint.pth")
+
+            # Write to the tfboard
+            writer.add_scalar('train/label-acc', label_acc.avg, step)
+            writer.add_scalar('train/unlabel-acc', unlabel_acc.avg, step)
+            writer.add_scalar('train/label-loss', label_losses.avg, step)
+            writer.add_scalar('train/unlabel-loss', unlabel_losses.avg, step)
+            writer.add_scalar('train/lr', lr, step)
+            writer.add_scalar('test/accuracy', acc, step)
+            if args.mix_up:
+                writer.add_scalar('train/interp-loss', interp_losses.avg, step)
+
+            # Reset AverageMeters
+            label_losses.reset()
+            unlabel_losses.reset()
+            label_acc.reset()
+            unlabel_acc.reset()
+            if args.mix_up:
+                interp_losses.reset()
+
+@torch.no_grad()
+def evaluate(test_loader, model):
+    batch_time, losses, acc = [AverageMeter() for _ in range(3)]
+    # switch to evaluate mode
+    model.eval()
+    end = time.time()
+    for i, (data, target) in enumerate(test_loader):
+        # Load data
+        if args.gpu:
+            data = data.cuda()
+            target = target.cuda()
+
+        # Compute output
+        pred = model(data)
+        loss = F.cross_entropy(pred, target, reduction='mean')
+
+        # Measure accuracy and record loss
+        top1, = accuracy(pred, target, topk=(1,))
+        losses.update(loss.item(), data.size(0))
+        acc.update(top1.item(), data.size(0))
+        # Measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        if i % args.print_freq == 0:
+            logger.info('Test: [{0}/{1}] Time {btime.val:.3f} (avg={btime.avg:.3f}) '
+                        'Test Loss {loss.val:.3f} (avg={loss.avg:.3f}) '
+                        'Acc {acc.val:.3f} (avg={acc.avg:.3f})' \
+                        .format(i, len(test_loader), btime=batch_time, loss=losses, acc=acc))
+
+    logger.info(' * Accuracy {acc.avg:.5f}'.format(acc=acc))
+    return acc.avg
+
+if __name__ == "__main__":
+    # Train and evaluate the model
+    torch.multiprocessing.freeze_support()
+    main()
+    writer.close()
+    logger.close()
